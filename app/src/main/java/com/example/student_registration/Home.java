9e2ca7d0a149fe122a085e620f5880615934be3c
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-12-23 10:36:05 +0300

    finished home structure

--- app/src/main/java/com/example/student_registration/Home.java
+++ app/src/main/java/com/example/student_registration/Home.java
@@ -2,7 +2,9 @@ package com.example.student_registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
     }
 }
--- app/src/main/java/com/example/student_registration/SplashScreen.java
+++ app/src/main/java/com/example/student_registration/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.student_registration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,6 +12,19 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },3000);
 
 
     }
