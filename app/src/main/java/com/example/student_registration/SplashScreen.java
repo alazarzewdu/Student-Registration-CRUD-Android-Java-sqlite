10956e45cbfbe365ce16864c4632aea184df2cfd
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-12-23 08:39:49 +0300

    Home activity created

---
+++ app/src/main/java/com/example/student_registration/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.example.student_registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
