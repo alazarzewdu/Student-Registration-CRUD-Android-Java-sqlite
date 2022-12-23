db96829c19f2dd68ede7c8cac8a2f558d19a800c
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-12-23 18:32:47 +0300

    frest start new me, here we go

--- app/src/main/java/com/example/student_registration/Home.java
+++ app/src/main/java/com/example/student_registration/Home.java
@@ -38,185 +38,30 @@ public class Home extends AppCompatActivity {
         bdelete = findViewById(R.id.btn_delete);
 
 
-        Intent i = getIntent();
-
-        String t1 = i.getStringExtra("id").toString();
-        String t2 = i.getStringExtra("fname".toString());
-        String t3 = i.getStringExtra("lname").toString();
-        String t4 = i.getStringExtra("age").toString();
-        String t5 = i.getStringExtra("department").toString();
-        String t6 = i.getStringExtra("section").toString();
-
-
-        eid.setText(t1);
-        efname.setText(t2);
-        elname.setText(t3);
-        eage.setText(t4);
-        edepartment.setText(t5);
-        esection.setText(t6);
-
-
-
-
-
-
-
-
-
 
 
         badd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    insert();
+                
             }
         });
 
-
-        bdisplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),Display.class);
-                startActivity(i);
-            }
-        });
-
-
-        bedit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                edit();
-            }
-        });
-
-
-        bdelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                delete();
-            }
-        });
 
 
     }
 
-    public void insert() {
-        try {
-            String id = eid.getText().toString();
-            String fname = efname.getText().toString();
-            String lname = elname.getText().toString();
-            String age = eage.getText().toString();
-            String department = edepartment.getText().toString();
-            String section = esection.getText().toString();
-
-
-            SQLiteDatabase db = openOrCreateDatabase("unity", Context.MODE_PRIVATE, null);
-            db.execSQL("CREATE TABLE IF NOT EXISTS student(id VARCHAR PRIMARY KEY, fname VARCHAR, lname VARCHAR, age INTEGER, department VARCHAR, section VARCHAR)");
-
-            String sql = "INSERT INTO student(id,fname,lname,age,department,section)values(?,?,?,?,?,?)";
-            SQLiteStatement statement = db.compileStatement(sql);
-            statement.bindString(1,id);
-            statement.bindString(2,fname);
-            statement.bindString(3,lname);
-            statement.bindString(4,age);
-            statement.bindString(5,department);
-            statement.bindString(6,section);
-            statement.execute();
-            Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show();
-
-            eid.setText("");
-            efname.setText("");
-            elname.setText("");
-            eage.setText("");
-            edepartment.setText("");
-            esection.setText("");
-            eid.requestFocus();
-
-
-        }
-        catch (Exception ex) {
-            Toast.makeText(this, "Failed Registration", Toast.LENGTH_SHORT).show();
-
-        }
-    }
 
 
 
     //edit
 
 
-    public void edit() {
-        try {
-            String id = eid.getText().toString();
-            String fname = efname.getText().toString();
-            String lname = elname.getText().toString();
-            String age = eage.getText().toString();
-            String department = edepartment.getText().toString();
-            String section = esection.getText().toString();
-
-
-            SQLiteDatabase db = openOrCreateDatabase("unity",Context.MODE_PRIVATE,null);
-
-            String sql = "update student set fname=?,lname=?,age=?,department=?,section=? where id=?";
-            SQLiteStatement statement = db.compileStatement(sql);
-            statement.bindString(1,id);
-            statement.bindString(2,fname);
-            statement.bindString(3,lname);
-            statement.bindString(4,age);
-            statement.bindString(5,department);
-            statement.bindString(6,section);
-            statement.execute();
-            Toast.makeText(this, "Record Updated", Toast.LENGTH_SHORT).show();
-
-            eid.setText("");
-            efname.setText("");
-            elname.setText("");
-            eage.setText("");
-            edepartment.setText("");
-            esection.setText("");
-            eid.requestFocus();
-
-
-        }
-        catch (Exception ex) {
-            Toast.makeText(this, "Failed Updating", Toast.LENGTH_SHORT).show();
-
-        }
-    }
-
 
     //delete
 
 
 
-    public void delete() {
-        try {
-            String id = eid.getText().toString();
-
-
-            SQLiteDatabase db = openOrCreateDatabase("unity", Context.MODE_PRIVATE, null);
-
-            String sql = "DELETE FROM student WHERE id = ?";
-            SQLiteStatement statement = db.compileStatement(sql);
-            statement.bindString(1,id);
-            statement.execute();
-            Toast.makeText(this, "Record Deleted", Toast.LENGTH_SHORT).show();
-
-            eid.setText("");
-            efname.setText("");
-            elname.setText("");
-            eage.setText("");
-            edepartment.setText("");
-            esection.setText("");
-            eid.requestFocus();
-
-
-        }
-        catch (Exception ex) {
-            Toast.makeText(this, "Failed Deleting", Toast.LENGTH_SHORT).show();
-
-        }
-    }
 
 
 }
