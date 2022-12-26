9f5535407e09b23ca3c42263e4136c6519a72e74
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-12-26 11:42:03 +0300

    finished function

--- app/src/main/java/com/example/student_registration/search.java
+++ app/src/main/java/com/example/student_registration/search.java
@@ -6,12 +6,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.nio.file.Paths;
@@ -63,15 +67,18 @@ public class search extends AppCompatActivity {
         }
         StringBuffer buffer = new StringBuffer();
         while(c.moveToNext()) {
-            buffer.append("" +c.getString(0)+"\n");
-            buffer.append("" +c.getString(1)+"\n");
-            buffer.append("" +c.getString(2)+"\n");
-            buffer.append("" +c.getString(3)+"\n");
-            buffer.append("" +c.getString(4)+"\n");
-            buffer.append("" +c.getString(5)+"\n");
+            buffer.append("Id: \t" +c.getString(0)+"\n");
+            buffer.append("First Name: \t" +c.getString(1)+"\n");
+            buffer.append("Last Name: \t" +c.getString(2)+"\n");
+            buffer.append("Age: \t" +c.getString(3)+"\n");
+            buffer.append("Department: \t" +c.getString(4)+"\n");
+            buffer.append("Section: \t" +c.getString(5)+"\n");
 
         }
-        Toast.makeText(getApplicationContext(), "Found \n" +buffer.toString() , Toast.LENGTH_SHORT).show();
+        Toast toast = Toast.makeText(getApplicationContext(), "Found \n" +buffer.toString(), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP | Gravity.LEFT, 300, 450);
+        toast.show();
+
 
 
     }
